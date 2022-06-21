42be23d1176fcd2e5c42237523f8c547413e2ee5
Queonias <queones@gmail.com>
2022-06-21 18:59:31 -0300

    Exercício 3

--- Exercicios/Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/scripts.js
+++ Exercicios/Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/scripts.js
@@ -1,12 +1,17 @@
-//Faça um programa que retorne o maior de dois números. Defina no começo do programa duas constantes com os valores que serão comparados.
+//Faça um programa que retorne o maior de três números. Defina no começo do programa três constantes com os valores que serão comparados.
 
-const a = 2;
-const b = 1;
+const a = 1;
+const b = 2;
+const c = 5;
 
-if (a > b) {
-  console.log(`O número ${a} é maior que o número ${b}.`);
-} else if (b > a) {
-  console.log(`O número ${b} é maior que o número ${a}.`);
-} else if (b === a) {
-  console.log(`O número ${b} é igual ao número ${a}.`);
+console.log(`Os números escolhidos foram:${a}, ${b}, ${c} `);
+
+if (a > b && a > c) {
+    console.log(`${a} é o maior número entre eles`);
+} else if (b > a && b > c) {
+    console.log(`${b} é o maior número entre eles`);
+} else if (c > a && c > b) {
+    console.log(`${c} é o maior número entre eles`);
+} else {
+    console.log("Há números com valores iguais.");
 }
