f45eb55a412b79b1c63b0e2f3dcb26099d41b976
Queonias <queones@gmail.com>
2022-06-21 18:48:50 -0300

    Exercício 2

--- Exercicios/Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/scripts.js
+++ Exercicios/Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/scripts.js
@@ -1,12 +1,12 @@
-const a = 1;
-const b = 2;
+//Faça um programa que retorne o maior de dois números. Defina no começo do programa duas constantes com os valores que serão comparados.
 
-console.log(`a =${a} ,b = ${b}`);
-console.log();
-console.log(`a + b = ${a + b}`);
-console.log(`a - b = ${a - b}`);
-console.log(`a * b = ${a * b}`);
-console.log(`a / b = ${a / b}`);
-console.log(`a % b = ${a % b}`);
+const a = 2;
+const b = 1;
 
-
+if (a > b) {
+  console.log(`O número ${a} é maior que o número ${b}.`);
+} else if (b > a) {
+  console.log(`O número ${b} é maior que o número ${a}.`);
+} else if (b === a) {
+  console.log(`O número ${b} é igual ao número ${a}.`);
+}
