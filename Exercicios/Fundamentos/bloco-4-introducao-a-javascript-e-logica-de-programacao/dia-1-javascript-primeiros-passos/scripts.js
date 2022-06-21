04e4812f28acb747b1db4a00d7591bd4c5995a79
Queonias <queones@gmail.com>
2022-06-21 18:38:08 -0300

    Exercício 1

---
+++ Exercicios/Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/scripts.js
@@ -0,0 +1,12 @@
+const a = 1;
+const b = 2;
+
+console.log(`a =${a} ,b = ${b}`);
+console.log();
+console.log(`a + b = ${a + b}`);
+console.log(`a - b = ${a - b}`);
+console.log(`a * b = ${a * b}`);
+console.log(`a / b = ${a / b}`);
+console.log(`a % b = ${a % b}`);
+
+
