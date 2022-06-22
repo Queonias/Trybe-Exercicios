41549c4e8eba611d6e4ed951002444c73c5808fb
Queonias <queones@gmail.com>
2022-06-22 18:36:27 -0300

    Exercício 8

---
+++ Exercicios/Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/Exercicio_8.js
@@ -0,0 +1,16 @@
+//  Escreva um programa que defina três números em constantes e retorne true se pelo menos uma das três for par. Caso contrário, ele retorna false.
+// Bonus: use somente um if.
+
+const num1 = 5;
+const num2 = 3;
+const num3 = 5;
+
+validadorNum1 = num1 % 2 === 0
+validadorNum2 = num2 % 2 === 0
+validadorNum3 = num3 % 2 === 0
+
+if (validadorNum1 || validadorNum2 || validadorNum3){
+    console.log("True");
+} else {
+    console.log("False");
+}
