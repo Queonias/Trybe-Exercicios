395d8021a8d2206487bee348ffa4ec0ff33deb04
queones <queones@gmail.com>
2022-08-19 16:10:12 -0300

    Exemplo 2

--- Exercicios/front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/aula1/src/App.js
+++ Exercicios/front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/aula1/src/App.js
@@ -1,26 +1,52 @@
 import './App.css';
 import React, { Component } from 'react';
 
-function handleClick() {
-  console.log('Clicou no botão!')
-}
-function handleClick2() {
-  console.log('Clicou no botão 2!')
-}
-function handleClick3() {
-  console.log('Clicou no botão 3!')
-}
+// function handleClick() {
+//   console.log('Clicou no botão!')
+// }
+// function handleClick2() {
+//   console.log('Clicou no botão 2!')
+// }
+// function handleClick3() {
+//   console.log('Clicou no botão 3!')
+// }
+// class App extends Component {
+//   render() {
+//     return (
+//       <>
+//       <button onClick={handleClick}>Meu botão</button>
+//       <button onClick={handleClick2}>Meu botão 2</button>
+//       <button onClick={handleClick3}>Meu botão 3</button>
+//       </>
+//       );
+//   }
+// }
+
+// export default App;
+
 class App extends Component {
+  // constructor() {
+  //   super()
+  //   // A função abaixo vincula "manualmente" o 'this' á nossa função
+  //   this.handleClick = this.handleClick.bind(this);// com essa implementação o javascript vai enchergar o this
+  // }
+
+  // handleClick() {
+  //   console.log(this); 
+  //   console.log('Clicou no botão!')
+  // }
+
+  handleClick = () => {
+    console.log(this); // essa é uma outra forma de se declarar um método sem usar o constructor 
+  }
+
   render() {
     return (
-      <>
-      <button onClick={handleClick}>Meu botão</button>
-      <button onClick={handleClick2}>Meu botão 2</button>
-      <button onClick={handleClick3}>Meu botão 3</button>
-      </>
-      );
+      <button type="button" onClick={this.handleClick}>Meu botão</button>
+    );
   }
 }
 
 export default App;
+
 
